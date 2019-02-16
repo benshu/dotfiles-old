6e004fc30e8a0c856ee320eb9e512c7e77e4ff88
Hagay Ben Shushan <hagay@intsights.com>
2019-02-16 14:56:52 +0200

    disabled display link support in monitors script
    now supporting simple hdmi / dp connections

--- monitors.sh
+++ monitors.sh
@@ -3,25 +3,28 @@
 # Get out of town if something errors
 set -e
 
-# HDMI_STATUS=$(</sys/class/drm/card0/card0-HDMI-A-1/status )  
-# DP_STATUS=$(</sys/class/drm/card0/card0-DP-1/status )
-HDMI_STATUS=$(</sys/class/drm/card2/card2-DVI-I-2/status )  
-DP_STATUS=$(</sys/class/drm/card1/card1-DVI-I-1/status )
+echo "starting"
+HDMI_STATUS=$(</sys/class/drm/card0/card0-HDMI-A-1/status )  
+DP_STATUS=$(</sys/class/drm/card0/card0-DP-1/status )
 
 if [ "$HDMI_STATUS" = "connected" ] && [ "$DP_STATUS" = "connected" ]; then  
-    xrandr --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DVI-I-2-2 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1 --off --output HDMI-2 --off
+    echo "xrandr --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --off --output HDMI2 --off"
+    xrandr --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --off --output HDMI2 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI and DP plugged in"
     exit
 elif [ "connected" = "$HDMI_STATUS" ]; then  
-    xrandr --output DVI-I-1-1 --off --output DVI-I-2-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off
+    echo "xrandr --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off"
+    xrandr --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI plugged in"
     exit
 elif [ "connected" = "$DP_STAUTS" ]; then  
-    xrandr --output DVI-I-2-2 --off --output DVI-I-1-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP-1 --mode 1920x1080 --pos 0x0 --output HDMI-2 --off
+    echo "xrandr --output HDMI1 --off --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --output HDMI2 --off"
+    xrandr --output HDMI1 --off --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --output HDMI2 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "VGA plugged in"
     exit
-else
-    xrandr --output eDP-1 --auto --output DVI-I-2-2 --off --output HDMI-2 --off --output DVI-I-1-1 --off
+else  
+    echo "xrandr --output eDP1 --auto --output HDMI1 --off --output HDMI2 --off --output DP1 --off"
+    xrandr --output eDP1 --auto --output HDMI1 --off --output HDMI2 --off --output DP1 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "External monitor disconnected"    
     exit
 fi
