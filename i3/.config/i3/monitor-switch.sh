0f063d449fcbb3c3e4fee38d49d296fac719c220
Hagay Ben Shushan <hagay@intsights.com>
2020-08-17 21:14:31 +0300

    monitor-switch script updated

--- i3/.config/i3/monitor-switch.sh
+++ i3/.config/i3/monitor-switch.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DIALOG_RESULT=$(echo -e 'auto\nsingle\ndual-monitor\nmirror\nHDMI-DP\nHDMI-2K' | rofi -dmenu -i -p "Choose monitors setup" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 13")
+DIALOG_RESULT=$(echo -e \
+    'auto\nsingle\ndual-monitor\nmirror\nHome-2K\nWork-dual\nexit' \
+    | rofi -dmenu -i -p "Choose monitors setup" -hide-scrollbar -tokenize -lines 6 -eh 1 -width 25 -location 0 -xoffset 0 -yoffset 0 -padding 20 -disable-history -font "ClearSansMedium 13")
 
 echo "This result is : $DIALOG_RESULT"
 sleep 1;
@@ -21,11 +23,11 @@ elif [ "$DIALOG_RESULT" = "mirror" ];
 then
     xrandr --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI plugged in"
-elif [ "$DIALOG_RESULT" = "HDMI-2K" ];
+elif [ "$DIALOG_RESULT" = "Home-2K" ];
 then
-    xrandr --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 2560x0 --rotate normal --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off
+    xrandr --output DP1 --off --output HDMI1 --mode 2560x1440 --pos 0x0 --rotate normal --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI plugged in"
-elif [ "$DIALOG_RESULT" = "HDMI-DP" ];
+elif [ "$DIALOG_RESULT" = "Work-dual" ];
 then
     xrandr --output DP1 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output eDP1 --off --output HDMI2 --off
     /usr/bin/notify-send --urgency=low -t 5000 "Graphics Update" "HDMI and DP plugged in"
